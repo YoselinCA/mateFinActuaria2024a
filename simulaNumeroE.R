5ae192cd5b2e863694b2081e612ee9e405a95299
YoselinCA <2350492h@umich.mx>
2024-08-29 13:03:41 -0600

    Add files via upload

---
+++ simulaNumeroE.R
@@ -0,0 +1,27 @@
+#Esta funci'on realiza el c'alculo y grafica de la aproximación del número e con t como argumento de entrada
+
+simulaNumeroE=function(nTotal){
+  #Creamos un objeto tipo data.frame con los cálculos del número e:
+  #Primero creamos los objetos que serán insumos para el cálculo de VF:
+  VA=1
+  i=1
+  t=1:nTotal
+  tablaNumeroe=data.frame(
+    t=t,
+    VF=VA*((1+(i/t))^t)
+  )
+  #Creamos un objeto tipo lista con la tabla del número e y algunos otros objetos:
+  objetoLista1=list(
+    tablaE=tablaNumeroe,
+    valorActual=VA,
+    tasaInteres=i,
+    periodosSimulados=nTotal
+  )
+  #Ahora grafico los valores de la tabla del número e:
+  
+  plot(x=tablaNumeroe$t,y=tablaNumeroe$VF,type="l",
+       col="brown")
+  
+  return(objetoLista1)
+  
+  }
